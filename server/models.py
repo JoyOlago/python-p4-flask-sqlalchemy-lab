41dbd89832085750680e99b8261eb2d302189e46
Joy Olago <joydanielle62@gmail.com>
2023-09-23 10:02:51 +0300

    assignment

--- server/models.py
+++ server/models.py
@@ -11,13 +11,22 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    birthday = db.Column(db.Date)
+    animals = db.relationship('Animal', backref='zookeeper', lazy='dynamic')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(128))
+    open_to_visitors = db.Column(db.Boolean)
+    animals = db.relationship('Animal', backref='enclosure', lazy='dynamic')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(128))
+    open_to_visitors = db.Column(db.Boolean)
+    animals = db.relationship('Animal', backref='enclosure', lazy='dynamic')
